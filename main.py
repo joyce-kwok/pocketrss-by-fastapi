41da2f1f79e34ddaea2877a4633c4d85ee96cfd1
joyce-kwok <joycekwokhy@gmail.com>
2025-03-29 14:59:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -113,9 +113,10 @@ def search_existing(source):
     print(f"Calling retrieve API to search saved posts, response code is {response.status_code}")
     if response.status_code == 200:
        articles = response.json()
-       last_item_key = next(reversed(data["list"]))  # Returns "4192836625"
-       last_item = data["list"][last_item_key] # Returns the full last item dict
+       last_item_key = next(reversed(articles['list']))  # Returns "4192836625"
+       last_item = articles["list"][last_item_key] # Returns the full last item dict
        latest = last_item['time_added']
+       print(f"Last updated: {latest}")
        for article in articles['list'].values():
            urlist.append(article['given_url'])
     else:
